306c68c306b1fe6eb9472cb850aa513b01d4ac90
Austin Bailey <baileyau@bc.edu>
2018-08-30 12:05:10 -0400

    setup

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'trimm-desktop-app'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
